3b38284d2a37ce7fe3f1a728a95efffa6617dc2f
shin5ok <shin5ok@55mp.com>
2018-08-17 01:27:38 +0800

    modify

--- index.py
+++ index.py
@@ -5,10 +5,11 @@ import os
 
 def handler(event, context):
     data = {
-        'ip': os.environ.get('REMOTE_ADDR', "None"),
+        # 'ip': os.environ.get('REMOTE_ADDR', "None"),
         'output': 'Hello World!!',
         'timestamp': datetime.datetime.utcnow().isoformat()
     }
     return {'statusCode': 200,
             'body': json.dumps(data),
+            'env': json.dumps(os.environ),
             'headers': {'Content-Type': 'application/json'}}
